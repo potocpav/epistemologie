0a0b4ad2da15c7b1dab6afe3374b5d6ffd74fae8
Pavel Potoček <pavelpotocek@gmail.com>
2022-11-03 23:04:43 +0100

    rename to epistemologie

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
             layers = [ ./content ];
             layersString = [ "./content" ];
             # port = 8080;
-            baseUrl = "/emanote-template/"; # Change to "/" (or remove it entirely) if using CNAME
+            baseUrl = "/epistemologie/"; # Change to "/" (or remove it entirely) if using CNAME
             # prettyUrls = true;
           };
         };
